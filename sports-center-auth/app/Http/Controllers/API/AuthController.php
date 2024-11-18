c17063ecd1205e7c4894f474cd25a88d57afb8ae
Ravindu-Vithanage <vithanageravindu@gmaIL.COM>
2024-11-18 12:43:57 +0530

    create Register,login,logout

---
+++ sports-center-auth/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Hash;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function userRegister(Request $request):JsonResponse{
+        
+        $validate=$request->validate([
+            "name"=>'required|string|max:255',
+            "email"=>'required|email|unique:users,email|max:255',
+            "password"=>'required|string|min:8'
+
+        ]);
+
+        $user=User::created([
+
+            "name"=>$validate['name'],
+            "email"=>$validate['email'],
+            "password"=>Hash::make($validate['password'])
+
+
+        ]);
+
+        if($user){
+           $token=$user->createToken('token-name')->plainTextToken;
+
+           return response()->json([
+                "message"=>"registation sucessfull",
+                "token_type"=>"bearer",
+                "token"=>$token
+
+
+           ]);
+        }else{
+
+            return response()->json([
+                "message"=>"registation not sucessfull somthing error",
+           ]);
+
+        }
+    }
+
+    public function userLogin(Request $request):jsonResponse{
+
+        $validate=$request->validate([
+            "email"=>'required|email|max:255',
+            "password"=>'required|string|min:8'
+
+        ]);
+
+        $user=User::where('email',$request->email)->first();
+
+        if(!$user || !Hash::check($request->password,$user->password)){
+
+            return response()->json([
+                "message"=>"invalid credintial",
+           ]);
+        }else{
+
+            $token=$user->createToken('token-name')->plainTextToken;
+
+            return response()->json([
+                 "message"=>"registation sucessfull",
+                 "token_type"=>"bearer",
+                 "token"=>$token
+ 
+ 
+            ]);
+        }
+    }
+    public function userLogout(Request $request):JsonResponse{
+
+        if($request->user()){
+            $request->user->tokens()->delete();
+            return response()->json([
+                "message"=>"Logout sucessfull",
+           ]);
+        }
+        return response()->json([
+            "message"=>"user not login",
+       ]);
+    }
+}
---
+++ sports-center-auth/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register',[AuthController::class,'userRegister']);
+Route::post('/login',[AuthController::class,'userLogin']);
+
+Route::middleware(['auth:sanctum'])->group(function(){
+
+    Route::post('/logout',[AuthController::class,'userLogout']);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
